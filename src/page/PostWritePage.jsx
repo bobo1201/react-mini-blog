292445965d4a94bf9d90feac72558e7daf5d86b4
bobo1201 <121841746+bobo1201@users.noreply.github.com>
2024-01-02 16:11:49 +0900

    리액트를 이용한 미니 블로그 완성

---
+++ src/page/PostWritePage.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import Button from "../ui/Button";
+import TextInput from "../ui/TextInput";
+
+const Wrapper = styled.div`
+    width: calc(100% - 32px);
+    padding: 16px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Container = styled.div`
+    width: 100%;
+    max-width: 720px;
+
+    :not(:last-child){
+        margin-bottom: 16px;
+    }
+`;
+
+function PostWritePage(props){
+    const navigete = useNavigate();
+
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+
+    return (
+        <Wrapper>
+            <Container>
+                <TextInput 
+                    height={20}
+                    value={title}
+                    onChange={(event) => {
+                        setTitle(event.target.value);
+                    }}
+                />
+                <TextInput 
+                    height={480}
+                    value={content}
+                    onChange={(event) => {
+                        setContent(event.target.value);
+                    }}
+                />
+                <Button
+                    title="글 작성하기"
+                    onClick={() => {
+                        navigete("/");
+                    }}
+                />
+            </Container>
+        </Wrapper>
+    )
+}
+
+export default PostWritePage;
